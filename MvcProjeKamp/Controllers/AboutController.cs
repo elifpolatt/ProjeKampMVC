d08562effa8b95670e04da81707e78ee0060a90e
Elif Polat <elif7polat@gmail.com>
2023-07-19 23:41:39 +0300

    about

--- MvcProjeKamp/Controllers/AboutController.cs
+++ MvcProjeKamp/Controllers/AboutController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +13,26 @@ namespace MvcProjeKamp.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
         public ActionResult Index()
         {
+            var aboutvalues = aboutManager.Getlist();
+            return View(aboutvalues);
+        }
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddAbout(About p) {
+            aboutManager.AboutAdd(p);
+            return RedirectToAction("Index");
+
+        }
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
         }
     }
 }
