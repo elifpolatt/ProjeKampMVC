4bf15d043db40acf2c784a781c78227567345e67
Elif Polat <elif7polat@gmail.com>
2023-07-22 18:00:36 +0300

    message-commit

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator() {
+            RuleFor(x => x.RecevierMail).NotEmpty().WithMessage("Alıcı mail adresini boş geçemezsiniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu boş geçemezsiniz.");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz.");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
+            RuleFor(x => x.RecevierMail).NotEmpty().WithMessage("E-posta adresi boş olamaz.").EmailAddress().WithMessage("Geçersiz e-posta adresi.");
+            RuleFor(x => x.Subject).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter giriniz");
+        }
+    }
+}
---
+++ MvcProjeKamp/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messagevalues = messageManager.GetListInbox();
+            return View(messagevalues);
+        }
+        public ActionResult SendBox()
+        {
+            var messagelist = messageManager.GetSendbox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+    }
+}
